110825f7fa21263a81bebf95b0b628c272206d51
Luiz Carlos Ortega Neto <luiz.ortega@ufms.com>
2021-06-15 17:26:51 -0400

    15/06 Início de algumas configurações

---
+++ app/src/main/java/com/example/trabalho2progmobile/aplicação/MainActivity.kt
@@ -0,0 +1,14 @@
+ package com.example.trabalho2progmobile.aplicação
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import com.example.trabalho2progmobile.R
+
+ class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
+}
---
+++ app/src/main/java/com/example/trabalho2progmobile/utils/BaseFragment.kt
@@ -0,0 +1,4 @@
+package com.example.trabalho2progmobile.utils
+
+class BaseFragment {
+}
