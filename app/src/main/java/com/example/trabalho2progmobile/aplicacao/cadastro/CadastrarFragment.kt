d6153170969d1679d48a9660cb87267cd89de8b4
Luiz Carlos Ortega Neto <luiz.ortega@ufms.com>
2021-06-21 19:54:21 -0400

    21/06 Tela de cadastro criada e com as verificações sendo feitas

--- app/src/main/java/com/example/trabalho2progmobile/aplicacao/cadastro/CadastrarFragment.kt
+++ app/src/main/java/com/example/trabalho2progmobile/aplicacao/cadastro/CadastrarFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.trabalho2progmobile.R
 import com.example.trabalho2progmobile.utils.BaseFragment
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.cadastrar_fragment.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CadastrarFragment: BaseFragment() {
@@ -21,7 +24,38 @@ class CadastrarFragment: BaseFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
+        requireActivity().toolbar.setTitle(R.string.cadastrar)
+        setupUi()
+        subscribeUi()
     }
 
+    private fun setupUi(){
+        btn_cadastrar.setOnClickListener {
+            _viewModel.verificarCampo(
+                input_nome_completo.text.toString(),
+                input_email.text.toString(),
+                input_password.text.toString(),
+                input_confirm_password.text.toString()
+            )
+        }
+    }
+
+    private fun subscribeUi(){
+        _viewModel.erroNome.observe(viewLifecycleOwner) {
+            mostrarErroDoMaskEditText(input_nome_completo, it)
+        }
+        _viewModel.erroEmail.observe(viewLifecycleOwner) {
+            mostrarErroDoMaskEditText(input_email, it)
+        }
+        _viewModel.erroSenha.observe(viewLifecycleOwner) {
+            mostrarErroDoMaskEditText(input_password, it)
+        }
+        _viewModel.erroConfirmarSenha.observe(viewLifecycleOwner) {
+            mostrarErroDoMaskEditText(input_confirm_password, it)
+        }
+    }
+
+    private fun mostrarErroDoMaskEditText(editText: TextInputEditText, erro: Int){
+        editText.error = getString(erro)
+    }
 }
--- app/src/main/java/com/example/trabalho2progmobile/utils/mvvm/viewModelModule.kt
+++ app/src/main/java/com/example/trabalho2progmobile/utils/mvvm/viewModelModule.kt
@@ -1,7 +1,22 @@
 package com.example.trabalho2progmobile.utils.mvvm
 
+import com.example.trabalho2progmobile.aplicacao.cadastro.CadastrarViewModel
+import com.example.trabalho2progmobile.aplicacao.inicial.InicialViewModel
+import com.example.trabalho2progmobile.aplicacao.login.LoginViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
 
+    viewModel {
+        InicialViewModel()
+    }
+
+    viewModel {
+        LoginViewModel()
+    }
+
+    viewModel {
+        CadastrarViewModel()
+    }
 }
